5593947047bbc17b772a45410c7ab0143008d73a
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-08-16 15:25:13 +0530

    Implement user and repository fetching on form submit
    Added functionality to fetch and display GitHub user data and repositories on form submission.

--- GitVerse/GV-v1/script.js
+++ GitVerse/GV-v1/script.js
@@ -1,1 +1,34 @@
+const form = document.querySelector("form");
+const usernameInput = document.querySelector("#username");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    getUser(usernameInput.value);
+    getRepos(usernameInput.value);
+});
+
+async function getUser(username) {
+
+    const response = await fetch(
+        `https://api.github.com/users/${username}`
+    );
+
+    if (!response.ok) {
+        console.log("User not found.");
+        return;
+    }
+    const data = await response.json();
+
+    console.log(data);
+}
+
+async function getRepos(username) {
+    const response = await fetch(
+        `https://api.github.com/users/${username}/repos`
+    );
+
+    const repos = await response.json();
+    repos.forEach((repo) => {
+        console.log(repo.name);
+    });
+}
