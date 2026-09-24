8a02728e89f559d018be4fe2f146389e0f40bbc1
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-09-24 23:07:00 +0530

    Update main.jsx

--- Stoic/src/main.jsx
+++ Stoic/src/main.jsx
@@ -1,3 +1,10 @@
+import '@fontsource/raleway/400.css'
+import '@fontsource/raleway/500.css'
+import '@fontsource/raleway/600.css'
+
+import '@fontsource/eb-garamond/500.css'
+import '@fontsource/eb-garamond/600.css'
+import './style.css'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
